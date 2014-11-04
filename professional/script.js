2d1c0edee99b60434c02094e31bb83631219450d
BrandonDGr8 <brandonthefirst597@gmail.com>
2014-11-04 02:35:39 -0500

    professional site

---
+++ professional/script.js
@@ -0,0 +1,45 @@
+var about_scroll = $(".about_scroll").offset().top;
+var projects_scroll = $(".projects_scroll").offset().top;
+var hire_scroll = $(".hire_scroll").offset().top;
+var contact_scroll = $(".contact_scroll").offset().top;
+
+var interval = setInterval(function() {
+    if ($(window).scrollTop() < projects_scroll) {
+        $(".about_button").addClass("underline");
+        $(".projects_button").removeClass("underline");
+        $(".hire_button").removeClass("underline");
+        $(".contact_button").removeClass("underline");
+        interval();
+    }
+    else if ($(window).scrollTop() >= projects_scroll && $(window).scrollTop() < hire_scroll) {
+        $(".about_button").removeClass("underline");
+        $(".projects_button").addClass("underline");
+        $(".hire_button").removeClass("underline");
+        $(".contact_button").removeClass("underline");
+        interval();
+    }
+    else if ($(window).scrollTop() >= hire_scroll && $(window).scrollTop() < contact_scroll) {
+        $(".about_button").removeClass("underline");
+        $(".projects_button").removeClass("underline");
+        $(".hire_button").addClass("underline");
+        $(".contact_button").removeClass("underline");
+        interval();
+    }
+    else if ($(window).scrollTop() >= contact_scroll) {
+        $(".about_button").removeClass("underline");
+        $(".projects_button").removeClass("underline");
+        $(".hire_button").removeClass("underline");
+        $(".contact_button").addClass("underline");
+        interval();
+    }
+}, 250);
+
+
+$(document).ready(function () {
+	$(".a").mouseenter(function () {
+		$(this).addClass("underline_two");
+	});
+	$(".a").mouseleave(function () {
+		$(this).removeClass("underline_two");
+	});
+});
