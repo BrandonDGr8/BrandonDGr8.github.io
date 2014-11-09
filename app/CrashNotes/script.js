f213e8da1bbd9e8a3c0812807ce168bb46f06565
BrandonDGr8 <brandonthefirst597@gmail.com>
2014-11-09 04:33:19 -0500

    cookies

--- app/CrashNotes/script.js
+++ app/CrashNotes/script.js
@@ -1,3 +1,7 @@
+//$.cookie("notes_array", JSON.stringify(notes_array));
+//var notes_array = $.parseJSON($.cookie("notes_array"));
+
+
 //note object
 function Note(text, title) {
 	this.text = text;
@@ -6,6 +10,7 @@ function Note(text, title) {
 
 //note array
 var notes_array = [];
+$.cookie("notes_array", JSON.stringify(notes_array));
 
 
 //saves notes and title to a new note object
@@ -13,7 +18,10 @@ function saveNote(note, title) {
 	var n = document.getElementById(note).value;
 	var t = document.getElementById(title).value;
 	var o = new Note(n, t);
+	$.cookie("notes_array", JSON.stringify(notes_array));
+	var notes_array = $.parseJSON($.cookie("notes_array"));
 	notes_array.push(o);
+	$.cookie("notes_array", JSON.stringify(notes_array));
 	//save note to cookie
 	//save title to corresponding cookie
 	// console.log(notes_array);
@@ -22,19 +30,23 @@ function saveNote(note, title) {
 
 
 function deleteNote() {
+	var notes_array = $.parseJSON($.cookie("notes_array"));
 	var index = notes_array.indexOf(current_note);
 	if (index > -1) {
 		notes_array.splice(index, 1);
 	}
+	$.cookie("notes_array", JSON.stringify(notes_array));
 	$(".new_text").remove();
 	loadTitlesToDropdown();
 }
 
 function loadTitlesToDropdown() {
 	$(".titles").remove();
+	var notes_array = $.parseJSON($.cookie("notes_array"));
 	for (var i = notes_array.length - 1; i >= 0; i--) {
 		$('select').append("<option class='titles' value=" + i + ">" + notes_array[i].title + "</option>");
 	};
+	$.cookie("notes_array", JSON.stringify(notes_array));
 	// var t = setTimeout(function(){loadTitlesToDropdown()},1000);
 }
 
@@ -42,15 +54,19 @@ var current_note = '';
 
 $("select").click(function () {
 	console.log("title clicked")
+	var notes_array = $.parseJSON($.cookie("notes_array"));
 	current_note = notes_array[$(this).val()];
+	$.cookie("notes_array", JSON.stringify(notes_array));
 	updateViewedText();
 });
 
 function updateViewedText() {
 	if (current_note !== null) {
 		$(".new_text").remove();
+		var notes_array = $.parseJSON($.cookie("notes_array"));
 		$('.loaded_text').append("<h2 class='header_title new_text'>" + current_note.title + "</h2>");
 		$('.loaded_text').append("<p class='viewed_text new_text'>" + current_note.text + "</p>");
+		$.cookie("notes_array", JSON.stringify(notes_array));
 	};
 };
 
