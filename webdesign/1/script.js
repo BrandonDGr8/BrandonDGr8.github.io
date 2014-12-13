1daf1b8dcb3872ba308794a0cd829eb7d2cd2eff
BrandonDGr8 <brandonthefirst597@gmail.com>
2014-12-13 16:38:07 -0500

    smooth scrolling

--- webdesign/1/script.js
+++ webdesign/1/script.js
@@ -43,3 +43,19 @@ $(document).ready(function () {
 		$(this).removeClass("underline_two");
 	});
 });
+
+$(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
