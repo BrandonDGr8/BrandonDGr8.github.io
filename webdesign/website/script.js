766dcb0547b2d0a5451eff8846bd8b914410c313
BrandonDGr8 <brandonthefirst597@gmail.com>
2015-03-14 23:21:05 -0400

    Testing Live Version of Redesign

---
+++ webdesign/website/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+	$("#menuButton").click(function(){
+		$("#menuButton").hide();
+		$("#closeButton").show("fast");
+		$("#navS").fadeTo("fast", 1);
+	});
+	$("#closeButton").click(function(){
+		$(this).hide("fast");
+		$("#menuButton").show();
+		$("#navS").fadeTo("fast", 0);
+		$("#navS").hide("fast");
+	});
+	$(".button").click(function(){
+		$("#closeButton").hide("fast");
+		$("#menuButton").show();
+		$("#navS").fadeTo("fast", 0);
+		$("#navS").hide("fast");
+	});
+
+	var intervalID;
+
+	function picLoops() { //cycles through the hackathon photos
+		var backImg = $('.hack-logos img:first-child');
+		backImg.siblings().hide();
+		backImg.remove();
+		$('.hack-logos').append(backImg);
+		backImg.fadeIn();
+	};
+
+	intervalID = setInterval(function(){picLoops();}, 2000);
+
+	var intervalIDm;
+
+	function picLoopsM() { //cycles through the mobile hackathon photos
+		var backImg = $('.hack-logos-m img:first-child');
+		backImg.siblings().hide();
+		backImg.remove();
+		$('.hack-logos-m').append(backImg);
+		backImg.fadeIn();
+	};
+
+	intervalIDm = setInterval(function(){picLoopsM();}, 2000);
+
+	$('.iContainer').mouseenter(function() {
+		if ($(this).hasClass('cEmail')) {
+			$('.contactText').hide();
+			$('.cEmail').show();
+		};
+		if ($(this).hasClass('cTwitter')) {
+			$('.contactText').hide();
+			$('.cTwitter').show();
+		};
+		if ($(this).hasClass('cGithub')) {
+			$('.contactText').hide();
+			$('.cGithub').show();
+		};
+		if ($(this).hasClass('cLinkedin')) {
+			$('.contactText').hide();
+			$('.cLinkedin').show();
+		};
+		if ($(this).hasClass('cResume')) {
+			$('.contactText').hide();
+			$('.cResume').show();
+		};
+
+	});
+
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
