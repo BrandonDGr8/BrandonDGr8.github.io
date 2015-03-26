d6f36bd00a8dd9f55e9e6eb32581ad50ba36208a
BrandonDGr8 <brandonthefirst597@gmail.com>
2015-03-26 17:47:48 -0400

    PlusAnchor for scroll, reorganized files and folders

--- js/script.js
+++ js/script.js
@@ -60,19 +60,3 @@ $(document).ready(function(){
 		};
 	});
 });
-//scroll effect to anchors
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
